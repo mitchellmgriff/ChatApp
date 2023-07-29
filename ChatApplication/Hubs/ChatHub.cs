b4484d5bab612d3ee9ddeb19543d1ca21964f74d
Mitch Griffiths <125629315+mitchellmgriff@users.noreply.github.com>
2023-07-29 14:39:07 -0700

    Add project files.

---
+++ ChatApplication/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+
+namespace ChatApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        }
+
+
+    }
+}
---
+++ ChatApplication/Models/UserMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApplication.Models
+{
+    public class UserMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public bool CurrentUser { get; set; }
+        public DateTime DateSent { get; set; }
+    }
+}
